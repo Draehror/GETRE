d0badf47a6645c251a299167f9c16ab17b6c011b
Draehror <jcma9000@gmail.comm>
2024-11-24 15:46:40 -0300

    telas

--- src/main/java/br/edu/vianna/getre/banco/ConnectionFactory.java
+++ src/main/java/br/edu/vianna/getre/banco/ConnectionFactory.java
@@ -1,4 +1,15 @@
 package br.edu.vianna.getre.banco;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","");
+
+    }
 }
